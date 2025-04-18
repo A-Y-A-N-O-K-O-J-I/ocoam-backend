110e91859e93edee39ad9b3f7e543bc8f61e6f8e
ayanokoji <ayanokojix2306@gmail.com>
2025-04-18 06:01:01 +0000

    fix

--- server.js
+++ server.js
@@ -5,13 +5,9 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 // hey
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/auth', authRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports=app 

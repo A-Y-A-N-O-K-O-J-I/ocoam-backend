06e2daa5695d7277658d810af89abd3b5a30488c
Ayanokoji Kiyotaka <ayanokojix2306@gmail.com>
2025-08-02 16:54:06 +0100

    m

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   const origin = req.get('Origin') || '';
-  if (!origin.includes('localhost:5173')) {
+  if (!origin.includes('api.oyocam.org')) {
     return res.status(403).send('Forbidden');
   }
   next();
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://api.oyocam.org",
   credentials: true
 }));
 app.use(cookieParser());
